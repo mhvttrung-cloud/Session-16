def080ce33ebb85e37b2c1375a923b12840bc3b0
trungvuong2103 <mhvttrung@gmail.com>
2025-12-14 22:26:56 +0700

    update . files

--- session16-1.js
+++ session16-1.js
@@ -96,18 +96,36 @@ alert(isEven(inp));
 // Viết hàm isPalindrome() để kiểm tra chuỗi đối xứng
 // Thực hiện thông báo cho người dùng bằng alert() có phải chuỗi đối xứng không
 
-function isPalindrome(sym){
-    for (let i = 0; i < sym.length / 2 ; i++){
+function isPalindrome(textInput){
+    for (let i = 0; i < textInput.length / 2 ; i++){
         //Muốn lấy giá trị đầu là sym[i]
         //Muốn lấy giá trị cuối là sym.[sym.length -1 -i]
-        if (sym[i] !== sym[sym.length -1 -i]){
+        if (textInput[i] !== textInput[textInput.length -1 -i]){
             return false;
         }
     }; return true;
 };  
     let textInput = prompt("こちらに、記号を入力してください！");
-console.log(isPalindrome(textInput), "là chuỗi đối xứng" );
+    if (false){
+        alert('không phải chuỗi đối xứng')
+    } else {
+        alert("Là chuỗi đối xứng");
+    }
 
 // đối xứng : Ví dụ 123 4 321; hoặc 123 b 321
 
 //Kết quả sao ra không có đúng. Mong được các thầy giải đáp ạ !
+
+
+// Tạo hàm tính tổng các phần tử trong mảng, với các mảng như sau:
+// (Gợi ý: có thể dùng reduce() trong javascript)
+
+function sum(total){
+    let reduce = arr5.reduce(function(acc, cur){
+        acc + cur;
+        return acc;
+}, 1);
+};
+const arr5 = [1, 2, 3, 4, 5, 6];
+const arr6 = [10, 20, 30, 40, 50];
+const arr7 = [1, 3, 5, 7, 9];
